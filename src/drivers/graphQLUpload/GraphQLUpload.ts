1b9c1ac25cde86ab30d8848e1e4ff5b3bbfe8daa
almeida <hello@eualmeida.com.br>
2026-07-22 18:54:52 -0300

    feat(uploads): add GraphQL upload storage

---
+++ src/drivers/graphQLUpload/GraphQLUpload.ts
@@ -0,0 +1,21 @@
+import { ASTNode, GraphQLError, GraphQLScalarType } from 'graphql';
+
+import { Upload } from './Upload';
+
+/**
+ * GraphQL scalar implementation for multipart file uploads.
+ */
+export const GraphQLUpload = new GraphQLScalarType({
+  name: 'Upload',
+  description: 'The `Upload` scalar type represents a file upload.',
+  parseValue(value: unknown) {
+    if (value instanceof Upload) return value.promise;
+    throw new GraphQLError('Upload value invalid.');
+  },
+  parseLiteral(node: ASTNode | ASTNode[]) {
+    throw new GraphQLError('Upload literal unsupported.', { nodes: node });
+  },
+  serialize() {
+    throw new GraphQLError('Upload serialization unsupported.');
+  },
+});
